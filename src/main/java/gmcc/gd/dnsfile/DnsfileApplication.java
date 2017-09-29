5aabc2a596cb6de77d36f15f77601c35af3bd6f2
linyuetian <linyuetian@gd.chinamobile.com>
2017-09-29 11:20:42 +0800

    Initial commit

---
+++ src/main/java/gmcc/gd/dnsfile/DnsfileApplication.java
@@ -0,0 +1,12 @@
+package gmcc.gd.dnsfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnsfileApplication.class, args);
+	}
+}
---
+++ src/test/java/gmcc/gd/dnsfile/DnsfileApplicationTests.java
@@ -0,0 +1,16 @@
+package gmcc.gd.dnsfile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DnsfileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
